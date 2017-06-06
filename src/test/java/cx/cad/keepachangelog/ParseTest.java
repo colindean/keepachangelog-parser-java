914522621033185c25245a744fa0f74c1d3a56a5
Colin Dean <git@cad.cx>
2017-06-06 12:35:17 -0400

    Simplifies tests by using BeforeClass initialization

--- src/test/java/cx/cad/keepachangelog/ParseTest.java
+++ src/test/java/cx/cad/keepachangelog/ParseTest.java
@@ -17,6 +17,7 @@ package cx.cad.keepachangelog;
 
 
 import org.junit.Assert;
+import org.junit.BeforeClass;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.junit.runners.JUnit4;
@@ -29,52 +30,41 @@ import java.util.Optional;
 @RunWith(JUnit4.class)
 public class ParseTest {
 
-    public static final Date DATE_2017_JUNE_6 = new Date(2017 - 1900, 5, 6); //XXX this is so bad
+    private static final Date DATE_2017_JUNE_6 = new Date(2017 - 1900, 5, 6); //XXX this is so bad
 
-    @Test
-    public void test_parser_instantiation() {
-        new ChangelogParser();
-    }
+    static ChangelogParser parser;
+    static ChangelogData data;
 
-    @Test
-    public void test_clean_parse() {
-        ChangelogParser parser = a_parser();
-        parser.parse(TestChangelogs.BASIC);
+    @BeforeClass
+    static public void setup() {
+        parser = new ChangelogParser();
+        data = parser.parse(TestChangelogs.BASIC);
     }
 
     @Test
     public void test_parser_gets_projectName() {
-        ChangelogParser parser = a_parser();
-        ChangelogData data = parser.parse(TestChangelogs.BASIC);
         Assert.assertEquals("project name", data.getProjectName());
     }
     @Test public void test_parser_gets_description() {
-        ChangelogParser parser = a_parser();
-        ChangelogData data = parser.parse(TestChangelogs.BASIC);
         Assert.assertTrue(data.getDescription().contains("semver.org"));
         Assert.assertTrue(data.getDescription().contains("notable changes"));
     }
     @Test public void test_parser_gets_entry_date() {
-        ChangelogParser parser = a_parser();
-        ChangelogData data = parser.parse(TestChangelogs.BASIC);
         Iterator<ChangelogEntry> it = data.getEntries().iterator();
         Assert.assertTrue(it.hasNext());
         Assert.assertEquals(DATE_2017_JUNE_6, it.next().getDate());
     }
     @Test public void test_parser_gets_entry_version() {
-        ChangelogParser parser = a_parser();
-        ChangelogData data = parser.parse(TestChangelogs.BASIC);
         Iterator<ChangelogEntry> it = data.getEntries().iterator();
         Assert.assertTrue(it.hasNext());
         Assert.assertEquals("1.1.0", it.next().getVersion());
     }
     @Test public void test_parser_gets_three_sections_for_1p0p0() {
-        ChangelogParser parser = a_parser();
-        ChangelogData data = parser.parse(TestChangelogs.BASIC);
         ChangelogEntry[] entries = data.getEntries().toArray(new ChangelogEntry[0]);
         ChangelogEntry lastEntry = entries[entries.length-1];
         Assert.assertEquals("1.0.0", lastEntry.getVersion());
         Assert.assertEquals(3, lastEntry.getSections().size());
+
         ChangelogSection[] sections = lastEntry.getSections().toArray(new ChangelogSection[0]);
         List<String> items = sections[1].getItems();
         Assert.assertEquals(2, items.size());
@@ -82,8 +72,6 @@ public class ParseTest {
 
     @Test
     public void test_parser_gets_changed_content() {
-        ChangelogParser parser = a_parser();
-        ChangelogData data = parser.parse(TestChangelogs.BASIC);
         ChangelogEntry[] entries = data.getEntries().toArray(new ChangelogEntry[0]);
         ChangelogEntry lastEntry = entries[entries.length-1];
         ChangelogSection[] sections = lastEntry.getSections().toArray(new ChangelogSection[0]);
@@ -93,20 +81,11 @@ public class ParseTest {
     }
     @Test
     public void test_parser_gets_specific_version() {
-        ChangelogParser parser = a_parser();
-        ChangelogData data = parser.parse(TestChangelogs.BASIC);
         Assert.assertEquals("1.1.0", data.getEntryForVersion("1.1.0").get().getVersion());
         Assert.assertEquals("1.0.0", data.getEntryForVersion("1.0.0").get().getVersion());
     }
     @Test
     public void test_parser_does_not_get_specific_version_when_missing() {
-        ChangelogParser parser = a_parser();
-        ChangelogData data = parser.parse(TestChangelogs.BASIC);
         Assert.assertEquals(Optional.empty(), data.getEntryForVersion("9.1.1"));
-    }
-
-
-    private ChangelogParser a_parser() {
-        return new ChangelogParser();
     }
 }
